e2c2ac722e7722e9a6a926a39c7fa56b4700368c
ganden16 <alvindian85@gmail.com>
2023-02-22 15:47:27 +0700

    first commit

---
+++ client/src/middleware/Authenticated.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useRecoilValue } from 'recoil'
+import { authenticated } from '../store'
+
+export default function Authenticated({children}) {
+   const navigate = useNavigate()
+   const auth = useRecoilValue(authenticated)
+
+   useEffect(() => {
+      if (!auth.check) {
+         navigate('/login')
+      }
+   }, [auth.check])
+   return (
+      children
+   )
+}
